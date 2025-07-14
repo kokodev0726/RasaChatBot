c4045dbb222c229a06a495cdabe26cd0d0a29377
Jerry-Cruz98 <DannCruz@protonmail.com>
2025-07-14 01:49:11 -0400

    fixed commit

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,27 @@
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
+import dotenv from 'dotenv';
 import * as schema from "@shared/schema";
+
+dotenv.config();
+
 
 neonConfig.webSocketConstructor = ws;
 
+console.log("sfdafasf");
+console.log( "Connecting to database at", process.env.DATABASE_URL);
 if (!process.env.DATABASE_URL) {
+  
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
   );
 }
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+pool.connect().then(() => {
+  console.log("Database connection successful");
+}).catch((err) => {
+  console.error("Database connection failed:", err);
+});
 export const db = drizzle({ client: pool, schema });
